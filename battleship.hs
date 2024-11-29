bb4a993938d23e5b7dbe85394b39dc008bbd5fa1
rainbowd-ash <146150247+rainbowd-ash@users.noreply.github.com>
2024-11-29 11:36:35 -0800

    build two boards, conditional ship hiding

--- battleship.hs
+++ battleship.hs
@@ -1,11 +1,23 @@
 module Main where
+
 import Setup
+
+debug :: Bool
+debug = False
 
 -- Print related stuff
 printBoard :: Board -> IO ()
 printBoard board = do
     putStrLn "  A B C D E F G H I J"
     mapM_ printRow (zip [1..] board)
+
+printHiddenBoard :: Board -> IO ()
+printHiddenBoard board = do
+    putStrLn "  A B C D E F G H I J"
+    mapM_ printRow (zip [1..] board)
+  where
+    printRow (rowNum, row) =
+        putStrLn $ (if rowNum < 10 then " " else "") ++ show rowNum ++ " " ++ unwords (map showHiddenSquare row)
 
 printRow :: (Int, [Square]) -> IO ()
 printRow (rowNum, row) = do
@@ -19,10 +31,35 @@ showSquare Ship  = "#"
 showSquare Hit   = "X"
 showSquare Miss  = "O"
 
+showHiddenSquare :: Square -> String
+showHiddenSquare Water = "~"
+showHiddenSquare Ship  = "~"  -- Hide ships in hidden view
+showHiddenSquare Hit   = "X"
+showHiddenSquare Miss  = "O"
+
 --
 
 main :: IO ()
 main = do
+    -- Create empty boards
     let emptyBoard = createBoard 10
-    boardWithShips <- placeShips emptyBoard [5, 4, 3, 3, 2]
-    printBoard boardWithShips
+    
+    -- Place ships for both players
+    playerBoard <- placeShips emptyBoard [5, 4, 3, 3, 2]
+    computerBoard <- placeShips emptyBoard [5, 4, 3, 3, 2]
+    
+    -- Display boards based on debug mode
+    if debug
+        then do
+            putStrLn "Player Board (Debug Mode):"
+            printBoard playerBoard
+            putStrLn "Computer Board (Debug Mode):"
+            printBoard computerBoard
+        else do
+            putStrLn "Player Board:"
+            printBoard playerBoard
+            putStrLn "Computer Board:"
+            printHiddenBoard computerBoard
+    
+    -- Proceed to game loop (to be implemented)
+    putStrLn "Game setup complete! BATTLE SHIP!"
